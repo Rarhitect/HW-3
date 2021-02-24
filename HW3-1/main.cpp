3ba6f3822a7250dc185dac801d1c03f765f0fa7f
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-25 01:56:39 +0300

    Написали класс для часов, чтобы конструктор запускал таймер, а метод выводил бы время работы

--- HW3-1/main.cpp
+++ HW3-1/main.cpp
@@ -1,14 +1,50 @@
-//
-//  main.cpp
-//  HW3-1
-//
-//  Created by Alyaev Roman on 24.02.2021.
-//
-
 #include <iostream>
+#include <chrono>
+#include <set>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <utility>
+#include <iomanip>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+class Time_check
+ {
+ public:
+     using steady_clock_type = std::chrono::steady_clock;
+     using time_point_type = steady_clock_type::time_point;
+     
+     Time_check() : the_begin(steady_clock_type::now()), the_duration(0) {}
+     ~Time_check()
+     {
+         auto end = steady_clock_type::now();
+     }
+     
+     void stop()
+     {
+         auto end = steady_clock_type::now();
+         auto duration = std::chrono::duration_cast <std::chrono::microseconds> (end - the_begin).count();
+         the_duration += duration;
+     }
+
+      void start()
+     {
+         the_begin = steady_clock_type::now();
+     }
+
+      auto time()
+     {
+         auto end = steady_clock_type::now();
+         auto duration = std::chrono::duration_cast <std::chrono::microseconds> (end - the_begin).count();
+         return duration;
+     }
+
+  private:
+     time_point_type the_begin;
+     long long the_duration;
+ };
+
+int main()
+{
+    
     return 0;
 }
