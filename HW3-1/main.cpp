78a05615d1bb4a9ce9ca2691d1180ef4ded1477e
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-25 02:25:07 +0300

    Реализовали все тесты и осуществили вывод на экран, проверяем теперь N

--- HW3-1/main.cpp
+++ HW3-1/main.cpp
@@ -34,7 +34,7 @@ class Time_check
       auto time()
      {
          auto end = steady_clock_type::now();
-         auto duration = std::chrono::duration_cast <std::chrono::microseconds> (end - the_begin).count();
+         auto duration = std::chrono::duration_cast <std::chrono::nanoseconds> (end - the_begin).count();
          return duration;
      }
 
@@ -45,6 +45,61 @@ class Time_check
 
 int main()
 {
+    const auto N = 100;
+    
+    std::set<int> s;
+    std::vector<int> v;
+    std::array<int, N> a;
+    
+    Time_check timer_1;
+    
+    for (auto i = 0; i < N; ++i)
+    {
+        s.insert(i + 1);
+    }
+    
+    auto test_1 = timer_1.time();
+    
+    std::cout << "The test with set-container is: " << test_1 << " ns" << std::endl;
+    
+    Time_check timer_2;
+    
+    for (auto i = N; i > 0; --i)
+    {
+        v.push_back(i+1);
+    }
+    std::sort(v.begin(), v.end());
+    
+    auto test_2 = timer_2.time();
+    
+    std::cout << "The test with vector-container is: " << test_2 << " ns" << std::endl;
+    
+    Time_check timer_3;
+    
+    for (auto i = N; i > 0; --i)
+    {
+        a[N - i] = i + 1;
+    }
+    std::sort(v.begin(), v.end());
+
+    auto test_3 = timer_3.time();
+    
+    std::cout << "The test with array-container is: " << test_3 << " ns" << std::endl;
+    
+    std::vector<long long> results{test_1, test_2, test_3};
+    std::sort(results.begin(), results.end());
+    if (results[0] == test_1)
+    {
+        std::cout << "The best is set with " << test_1 << " nanoseconds! Congrats :)" << std::endl;
+    }
+    else if (results[0] == test_2)
+    {
+        std::cout << "The best is vector with " << test_2 << " nanoseconds! Congrats :)" << std::endl;
+    }
+    else if (results[0] == test_3)
+    {
+        std::cout << "The best is array with " << test_3 << " nanoseconds! Congrats :)" << std::endl;
+    }
     
     return 0;
 }
