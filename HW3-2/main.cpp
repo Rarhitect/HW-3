ced8095469a89483ba6cdef55e71d941b547c141
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-25 10:32:00 +0300

    initial

--- HW3-2/main.cpp
+++ HW3-2/main.cpp
@@ -1,15 +1,7 @@
-//
-//  main.cpp
-//  HW3-2
-//
-//  Created by Alyaev Roman on 25.02.2021.
-//
-
 #include <iostream>
 
 int main()
 {
-    int a = 9;
-    std::cout << "Hello " << a << std::endl;
+    
     return 0;
 }
