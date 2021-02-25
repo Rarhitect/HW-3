f1c4cd73df62356e4c7fea08c87c3318ab99d580
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2021-02-25 20:38:49 +0300

    initial

--- HW3-5/main.cpp
+++ HW3-5/main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  HW3-5
-//
-//  Created by Alyaev Roman on 25.02.2021.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    
     return 0;
 }
